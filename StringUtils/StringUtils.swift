da8861cb26581663a79cd83766bffe416769b059
KevinCarm <kevincarm016@hotmail.com>
2025-07-09 23:08:57 -0600

    Initial Commit

---
+++ StringUtils/StringUtils.swift
@@ -0,0 +1,9 @@
+//
+//  StringUtils.swift
+//  StringUtils
+//
+//  Created by Kevin Carmona Serrano on 09/07/25.
+//
+
+import Foundation
+
---
+++ StringUtilsTests/StringUtilsTests.swift
@@ -0,0 +1,17 @@
+//
+//  StringUtilsTests.swift
+//  StringUtilsTests
+//
+//  Created by Kevin Carmona Serrano on 09/07/25.
+//
+
+import Testing
+@testable import StringUtils
+
+struct StringUtilsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
